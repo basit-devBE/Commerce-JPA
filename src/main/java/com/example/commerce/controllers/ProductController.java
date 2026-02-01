5e60a782daa859455c49b6398361e41f18d8c8b4
basit-devBE <basitmohammed362@gmail.com>
2026-02-01 17:30:57 +0000

    refactor: Remove unused import for Page in ProductController

--- src/main/java/com/example/commerce/controllers/ProductController.java
+++ src/main/java/com/example/commerce/controllers/ProductController.java
@@ -14,7 +14,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
